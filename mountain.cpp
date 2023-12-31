be1867ce682463a979ea28f41eef38a2f0eca0df
Tran Cong Minh <tc.tranminh@gmail.com>
2023-12-31 17:02:22 +0700

    styling

--- mountain.cpp
+++ mountain.cpp
@@ -9,7 +9,8 @@
 #define MAXBYTES (1 << 27)
 #define MAXSTRIDE 32
 #define STRIDESTRIDE 2
-#define MAXELEMS MAXBYTES/sizeof(int)
+//#define MAXELEMS MAXBYTES/sizeof(int)
+#define MAXELEMS (1 << 27) / sizeof(long)
 
 std::vector<int> data(MAXELEMS);
 
